bf4e0c408a0f944b450e1b1ba76095744b1dfbf4
Dekay <dannykumah27@gmail.com>
2022-12-25 10:14:02 +0000

    PHP Authentication (unfinished)
    -- logon.php added for database connection
    -- connected to database through IDE

---
+++ logon.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$conn = mysqli_connect('localhost', 'root', '', 'phpgrid');
+
+/*
+ *
+if(!$conn)
+{
+    die('Could not Connect MySql Server:' . mysqli_error($conn));
+}
+*/
+
+
+/*
+ *
+if($conn){
+    echo "We made it";
+}
+
+*/
